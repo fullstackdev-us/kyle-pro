ac4dbfd4d8e340d79cd585656c4d4c70e577b792
fullstackdev-us <5263943+fullstackdev-us@users.noreply.github.com>
2023-04-07 15:07:23 -0700

    Working on the resume form

--- src/components/request-resume-form.vue
+++ src/components/request-resume-form.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="resume-form">
-    <button class="btn btn-primary">Request Resume</button>
-    <!-- <form v-if="showRequestForm" @submit.prevent="requestResume">
-      <label>Your Email Address:</label>
-      <input type="email" v-model="requestEmailAddress">
-      <button type="submit" :disabled="!requestEmailAddress">Submit</button>
-    </form> -->
+    <button v-if="!requesting" @click="requestResume" class="btn btn-primary">Request Resume</button>
+    <div class="grid gap-3" v-else>
+      <input  v-model="email" type="text" placeholder="Email Address" class="input input-bordered w-full" />
+      <div class="grid grid-cols-2 gap-2">
+        <button @click="requestResume" class="btn btn-primary">Submit</button>
+        <button @click="cancelRequestResume" class="btn btn-primary">Cancel</button>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref('')
+const requesting = ref(false);
+
+const requestResume = () => {
+  requesting.value = true;
+}
+
+const cancelRequestResume = () => {
+  requesting.value = false;
+}
+
+const submitEmail = () => {
+  console.log(email.value)
+  requesting.value = false;
+}
+</script>
 
 <style lang="scss">
 .resume-form {
